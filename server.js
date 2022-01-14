5fe627604e1f31071302340db880c9bbae2df96e
George Hazboun <thenoob885@gmail.com>
2022-01-14 11:56:10 +0200

    prepare for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -10,7 +11,7 @@ connectDB();
 //init middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('Server Running'));
+// app.get('/', (req, res) => res.send('Server Running'));
 
 //tried deploying to heroku (failed)
 
@@ -24,6 +25,15 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
 
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`server is listening on port ${PORT}`));
